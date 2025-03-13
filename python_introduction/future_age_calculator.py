e1e588739afb1e7839898b8ce16ede109b803133
Isaac Mwangale <isaacmario.m1@gmail.com>
2025-03-14 02:41:04 +0300

    Added new files to the directory

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,11 @@
+current_year = 2023
+future_year = 2050
+
+age = int(input("How old are you?"))
+
+born_year = current_year - age
+
+future_age = future_year - born_year
+
+print(f"In 2050, you will be {future_age} years old")
+
---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,6 @@
+hours = 2
+
+# calculating the hours in seconds 
+seconds = int(hours * 3600)
+
+print(f"{hours} hour(s) is {seconds} seconds")
