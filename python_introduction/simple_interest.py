c28088632c0f3d44e30eba74a9052a6d4f806b70
Isaac Mwangale <isaacmario.m1@gmail.com>
2025-03-14 10:33:13 +0300

    changing from words to letters

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,7 +1,7 @@
-principal = 1000
-rate = 0.05
-time = 3
+principal = P = 1000
+rate = R = 0.05
+time = T = 3
 
-interest = float(principal * rate * time)
+I = float(P * R * T)
 
-print("The simple interest is: ", interest)
+print("The simple interest is: ", I)
