6e81207a643f4e2830cf2bafab410b93465cc390
Isaac Mwangale <isaacmario.m1@gmail.com>
2025-04-04 21:36:24 +0300

    creating calculator type

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,13 @@
+class Calculator:
+    calculation_type = "Arithmetic Operations"
+
+    # Static method
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    # Class method
+    @classmethod
+    def multiply(cls, a, b):
+        print(f"Calculation type: {cls.calculation_type}")
+        return a * b
