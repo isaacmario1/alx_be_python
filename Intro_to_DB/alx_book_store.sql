7ce827c803f0213d3aa7aa2bb9cee24fbbc10bde
Isaac Mwangale <isaacmario.m1@gmail.com>
2025-04-16 18:27:52 +0300

    Fix: Add CREATE DATABASE IF NOT EXISTS alx_book_store

--- Intro_to_DB/alx_book_store.sql
+++ Intro_to_DB/alx_book_store.sql
@@ -1,13 +1,14 @@
-CREATE DATABASE ALX_BOOK_STORE;
-
+CREATE DATABASE IF NOT EXISTS ALX_BOOK_STORE;
 USE ALX_BOOK_STORE;
 
-CREATE TABLE Authors (
+-- Create Authors table
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT PRIMARY KEY,
     author_name VARCHAR(215)
 );
 
-CREATE TABLE Books (
+-- Create Books table
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT PRIMARY KEY,
     title VARCHAR(130),
     author_id INT,
@@ -16,21 +17,24 @@ CREATE TABLE Books (
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
-CREATE TABLE Customers (
+-- Create Customers table
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
 
-CREATE TABLE Orders (
+-- Create Orders table
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE Order_Details (
+-- Create Order_Details table
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT PRIMARY KEY,
     order_id INT,
     book_id INT,
